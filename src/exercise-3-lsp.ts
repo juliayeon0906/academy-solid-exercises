527338b9e7fbe09d1807e40265d6a4b66a495ab2
juliayeon0906 <julia.yeon@kainos.com>
2026-06-17 15:14:30 -0300

    exercise 3 is completed

--- src/exercise-3-lsp.ts
+++ src/exercise-3-lsp.ts
@@ -12,9 +12,44 @@
 // Run:  npm run exercise-3
 // =============================================================
 
-class Rectangle {
-  protected width: number = 0;
-  protected height: number = 0;
+// class Rectangle {
+//   protected width: number = 0;
+//   protected height: number = 0;
+
+//   setWidth(w: number) {
+//     this.width = w;
+//   }
+
+//   setHeight(h: number) {
+//     this.height = h;
+//   }
+
+//   area(): number {
+//     return this.width * this.height;
+//   }
+// }
+
+// class Square extends Rectangle {
+//   setWidth(w: number) {
+//     this.width = w;
+//     this.height = w; // forces equal sides
+//   }
+
+//   setHeight(h: number) {
+//     this.width = h;
+//     this.height = h; // forces equal sides
+//   }
+// }
+
+interface Shape {
+  area(): number;
+}
+
+class Rectangle implements Shape {
+  constructor(
+    private width: number = 0, 
+    private height: number = 0
+  ) {}
 
   setWidth(w: number) {
     this.width = w;
@@ -29,26 +64,29 @@ class Rectangle {
   }
 }
 
-class Square extends Rectangle {
-  setWidth(w: number) {
-    this.width = w;
-    this.height = w; // forces equal sides
-  }
+class Square implements Shape {
+  constructor(
+    private side: number = 0
+  ){}
 
-  setHeight(h: number) {
-    this.width = h;
-    this.height = h; // forces equal sides
+  setSide(s: number) {
+    this.side = s;
+  }
+  
+  area(): number {
+    return this.side * this.side;
   }
 }
 
-function printArea(shape: Rectangle) {
-  shape.setWidth(5);
-  shape.setHeight(10);
+function printArea(shape: Shape) {
   console.log(`Expected area: 50, Got: ${shape.area()}`);
 }
 
 const rect = new Rectangle();
+rect.setWidth(5);
+rect.setHeight(10);
 printArea(rect); // ✅ Expected area: 50, Got: 50
 
 const square = new Square();
-printArea(square); // ❌ Expected area: 50, Got: 100
+square.setSide(5);
+printArea(square); // ✅ Expected area: 25
